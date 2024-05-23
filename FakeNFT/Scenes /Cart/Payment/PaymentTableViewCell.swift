125ef1b7032fa7b56a9bdc94e75807d41b8a7af7
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2024-05-23 11:41:14 +0800

    Got started on the PaymentTableViewCell layout

--- FakeNFT/Scenes /Cart/Payment/PaymentTableViewCell.swift
+++ FakeNFT/Scenes /Cart/Payment/PaymentTableViewCell.swift
@@ -8,5 +8,65 @@
 import UIKit
 
 final class PaymentTableViewCell: UITableViewCell {
+    static let identifier = "PaymentTableViewCell"
     
+    private let iconImage: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = 6
+        return image
+    }()
+    
+    private let currencyNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .yaBlackLight
+        label.font = .caption2
+        return label
+    }()
+    
+    private let currencyCodeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .yaGreen
+        label.font = .caption2
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .yaLightGrayLight
+        layer.cornerRadius = 16
+        
+        addSubviews()
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: UI Layout
+extension PaymentTableViewCell {
+    private func addSubviews() {
+        let subviews = [iconImage, currencyNameLabel, currencyCodeLabel]
+        subviews.forEach { item in
+            item.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(item)
+        }
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            iconImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            iconImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            iconImage.heightAnchor.constraint(equalToConstant: 36),
+            iconImage.widthAnchor.constraint(equalToConstant: 36),
+            
+            currencyNameLabel.leadingAnchor.constraint(equalTo: iconImage.leadingAnchor, constant: 4),
+            currencyNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            
+            currencyCodeLabel.leadingAnchor.constraint(equalTo: currencyNameLabel.leadingAnchor),
+            currencyCodeLabel.topAnchor.constraint(equalTo: currencyNameLabel.bottomAnchor)
+        ])
+    }
 }
