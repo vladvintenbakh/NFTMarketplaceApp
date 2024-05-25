799522aed8aeae6d80bbfc00e4b9027da3cb2290
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2024-05-25 10:40:05 +0800

    Added the CartItem and PaymentCurrency models

---
+++ FakeNFT/Models/CartItem.swift
@@ -0,0 +1,16 @@
+//
+//  CartItem.swift
+//  FakeNFT
+//
+//  Created by Vlad Vintenbakh on 25/5/24.
+//
+
+import Foundation
+
+struct CartItem {
+    let id: String
+    let title: String
+    let imageName: String
+    let rating: Int
+    let price: Double
+}
