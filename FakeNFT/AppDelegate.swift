433d7fd43f80d01cf7a6825fdf66492cd193c366
SergeyVSwift <vashhenkobs@gmail.com>
2024-06-08 10:45:11 +0200

    no message

--- FakeNFT/AppDelegate.swift
+++ FakeNFT/AppDelegate.swift
@@ -1,28 +1,3 @@
-/*import UIKit
- 
- @main
-final class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-    
-    func application(
-        _: UIApplication,
-        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        let servicesAssembly = ServicesAssembly(
-            networkClient: DefaultNetworkClient(),
-            nftStorage: NftStorageImpl()
-        )
-        
-        let tabBarController = TabBarController()
-        tabBarController.servicesAssembly = servicesAssembly
-        
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = tabBarController
-        window?.makeKeyAndVisible()
-        
-        return true
-    }
-}*/
 import UIKit
 
 @main
