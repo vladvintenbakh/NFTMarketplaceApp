faa86c0450f415ecb0d125dfd1850ccc2e949afa
SergeyVSwift <vashhenkobs@gmail.com>
2024-06-08 10:44:41 +0200

    no message

--- FakeNFT/AppDelegate.swift
+++ FakeNFT/AppDelegate.swift
@@ -1,4 +1,4 @@
-import UIKit
+/*import UIKit
  
  @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,6 +21,44 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         
         return true
+    }
+}*/
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+        window = UIWindow()
+        isNeedToShowOnboarding()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    private func isNeedToShowOnboarding() {
+        if UserDefaults.standard.object(forKey: ConstantsCatalog.onboardingKey) == nil {
+            window?.rootViewController = setupTabBar()
+        } else {
+            let tabBarController = setupTabBar()
+            window?.rootViewController = tabBarController
+        }
+    }
+
+    func setupTabBar() -> UITabBarController {
+        let servicesAssembly = ServicesAssembly(
+            networkClient: DefaultNetworkClient(),
+            nftStorage: NftStorageImpl()
+        )
+
+        let tabBarController = TabBarController()
+        tabBarController.servicesAssembly = servicesAssembly
+        return tabBarController
     }
 }
  
